4d22705409c34a638602f17f4f4cb939c340301c
Aleksandr Ivanov <avic.ivanov@gmail.com>
2025-11-19 19:44:39 +0400

    Fix S3 bucket name in setup.sh: runpopds -> runpods

--- setup.sh
+++ setup.sh
@@ -78,7 +78,7 @@ echo "Downloading s3_download.py script..."
 wget -O /app/s3_download.py https://raw.githubusercontent.com/Wolverinoid/runpods-comfy/refs/heads/main/s3_download.py
 chmod +x /app/s3_download.py
 
-/app/installvenv/bin/python /app/s3_download.py runpopds/comfy.tar.gz /app/ --workers 12 --chunk-size 64
+/app/installvenv/bin/python /app/s3_download.py runpods/comfy.tar.gz /app/ --workers 12 --chunk-size 64
 
 cd /app
 
